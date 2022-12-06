95986b1fde0aa3d2be026159d44503fc892fb81e
tuandat333111 <tuandattuandat123@gmail.com>
2022-12-06 09:50:11 +0700

    update code

--- Rest/js/main.js
+++ Rest/js/main.js
@@ -1,27 +1,25 @@
 const calAverage=(...numbers)=>{
-    let sum=0;
-    let average=0;
-    numbers.forEach((number)=>{
-        sum+=number;
-    })
-    
-    average=sum/numbers.length;
-    console.log(sum,numbers.length,average);
+    let total=numbers.reduce((total,number)=>total+=number,0);
+    let n=numbers.length;
+    let average=total/n;
     return average;
 }
-document.getElementById("btnKhoi1").addEventListener("click",()=>{
-    let toan1=document.getElementById("inpToan").value*1;
-    let ly1=document.getElementById("inpLy").value*1;
-    let hoa1=document.getElementById("inpHoa").value*1;
-    let result=calAverage(toan1,ly1,hoa1);
-    document.getElementById("tbKhoi1").innerHTML=result;
+const getEle=(id)=>document.getElementById(id);
+
+calAverage(1,2,3);
+getEle("btnKhoi1").addEventListener("click",()=>{
+    let toan=getEle("inpToan").value*1;
+    let ly=getEle("inpLy").value*1;
+    let hoa=getEle("inpHoa").value*1;
+    let result=calAverage(toan,ly,hoa);
+    getEle("tbKhoi1").innerHTML=result;
 })
 
-document.getElementById("btnKhoi2").addEventListener("click",()=>{
-    let van=document.getElementById("inpVan").value*1;
-    let su=document.getElementById("inpSu").value*1;
-    let dia=document.getElementById("inpDia").value*1;
-    let tienganh=document.getElementById("inpEnglish").value*1;
+getEle("btnKhoi2").addEventListener("click",()=>{
+    let van=getEle("inpVan").value*1;
+    let su=getEle("inpSu").value*1;
+    let dia=getEle("inpDia").value*1;
+    let tienganh=getEle("inpEnglish").value*1;
     let result=calAverage(van,su,dia,tienganh);
-    document.getElementById("tbKhoi2").innerHTML=result;
+    getEle("tbKhoi2").innerHTML=result;
 })
