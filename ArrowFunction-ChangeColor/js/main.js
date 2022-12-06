d4241e227ca0b0a0ab2df5a3ddf711d5117cbf0e
tuandat333111 <tuandattuandat123@gmail.com>
2022-12-06 17:16:24 +0700

    Fix exercise 1

--- ArrowFunction-ChangeColor/js/main.js
+++ ArrowFunction-ChangeColor/js/main.js
@@ -1,18 +1,27 @@
 const colorList=["pallet","viridian","pewter","cerulean","vermillion","lavender",
 "celadon","saffron","fuschia","cinnabar"];
-console.log(colorList);
+
 const renderHTML=(data)=>{
-    let content=""
-    data.forEach((color)=>{
-        content+=`<button class="color-button ${color}" onclick="changeColor('${color}')"></button>`
+    let content="";
+    data.forEach((color,index)=>{
+        console.log(index);
+        content+= 0 == index?`<button class="color-button ${color} active" onclick="changeColor('${color}','${index}')"></button>`:
+        `<button class="color-button ${color}" onclick="changeColor('${color}','${index}')"></button>`;
     });
     document.getElementById("colorContainer").innerHTML=content;
+    
+    
 }
 renderHTML(colorList);
-
-const changeColor=(color)=>{
+const changeColor=(color,index)=>{
     document.getElementById("house").classList="house";
-    document.getElementById("house").classList.add(`${color}`);
+    document.getElementById("house").classList.add(`${color}`);   
+
+    let current=document.getElementsByClassName("active");
+    current[0].className=current[0].className.replace("active","");       
+    document.getElementsByClassName("color-button")[index].classList.add("active");
+    
 }
 window.changeColor=changeColor;
+
 
